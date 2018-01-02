d06196b071724fa2afbb82bd391b5f23720ebf85
s-linji <s-linji@bsd405.org>
2018-01-02 13:37:11 -0800

    ejafoiwnvdo check 2

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -5,7 +5,7 @@ package textExcel;
 public class Spreadsheet implements Grid
 {
 	private Cell[][] cellz;
-	//Spreadsheet constructor Checkpoint 1
+//Spreadsheet constructor Checkpoint 1
 	public Spreadsheet()
 	{
 		/*
@@ -21,36 +21,28 @@ public class Spreadsheet implements Grid
 			}
 		}
 	}
-	
-	@Override
-	public String processCommand(String command)
+	/*public String processCommand(String command)
 	{
+		//from Checkpoint 1, DO NOT USE!
 		return "";
-	}
-
-	@Override
+	}*/
 	public int getRows() //must be correct for Ch1
 	{
 		return 20;
 	}
-
-	@Override
 	public int getCols() //must be correct for Ch1 
 	{
 		return 12;
 	}
-
-	@Override
 	public Cell getCell(Location loc)
 	{
 		return null;
 	}
-
-	@Override
-	public String getGridText()
+	/*public String getGridText()
 	{
+		//From checkpoint 1, do NOT USE!
 		return null;
-	}
+	}*/
 	
 	// You are free to use this helper method.  It takes a column letter (starting at "A")
 	// and returns the column number corresponding to that letter (0 for "A", 1 for "B", etc.)  
@@ -60,7 +52,6 @@ public class Spreadsheet implements Grid
 	{
 		return Character.toUpperCase(columnLetter.charAt(0)) - 'A';
 	}
-
 	// You are free to use this helper method.  It takes a column number (starting at 0)
 	// and returns the column letter corresponding to that number ("A" for 0, "B" for 1, etc.)
 	// WARNING!!  If the parameter is not a number in the range of your Spreadsheet,
@@ -69,4 +60,37 @@ public class Spreadsheet implements Grid
 	{
 		return "" + (char) ('A' + columnNumber);
 	}
+	
+// Checkpoint 2 methods
+	public String getGridText()
+	{
+		//12 col x 20 row
+		/* Return a string containing the entire sheet 
+		 * grid in the form described in the spec.
+		*/
+		String grid = " ";
+		return null;
+	}
+	public String processCommand(String command)
+	{
+		//cell inspection. This should return the value at that cell.
+		if (command.equals("clear"))
+		{
+			//clear the entire grid
+			
+		}
+		else if (command.length() <= 3) //if just "A1" or "C15"
+		{
+			//cell inspection
+			
+		}
+		else if (command.indexOf("clear ") != -1) //clear with a space
+		{
+			//clearing a particular cell
+			
+		}
+		return "";
+	}
+	
+	
 }
---
+++ TextExcel/src/textExcel/TextCell.java
@@ -0,0 +1,20 @@
+package textExcel;
+
+//Checkpoint 2 TextCell class
+public class TextCell implements Cell
+{
+	//A TextCell stores string values.
+	private String string = "";
+	
+	public String abbreviatedCellText()
+	{
+		
+		return null;
+	}
+	public String fullCellText()
+	{
+		
+		return null;
+	}
+	
+}

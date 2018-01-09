5350cacf518641221f847ee8b80c27cf8ace70ce
s-linji <s-linji@bsd405.org>
2018-01-09 08:10:52 -0800

    ???????

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -25,7 +25,7 @@ public class Spreadsheet implements Grid
 	}
 	public Cell getCell(Location loc)
 	{
-		if (!cellz[loc.getRow()][loc.getCol()].equals("")) 
+		if (!cellz[loc.getRow()][loc.getCol()].equals("          ")) 
 		{
 			return cellz[loc.getRow()][loc.getCol()];
 		}

74fe8fe9f6f1e31f28cbe520159d2dbfb522f1f2
s-linji <s-linji@bsd405.org>
2018-01-09 15:13:19 -0800

    ???DE?QCQVEV!EV

--- TextExcel/src/textExcel/SpreadsheetLocation.java
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -21,7 +21,7 @@ public class SpreadsheetLocation implements Location
     //Constructor for Checkpoint 1
     public SpreadsheetLocation(String cellName)
     {
-    	this.row = Integer.parseInt(cellName.substring(1)) + 1; //ex: B2's row index is 1 
+    	this.row = Integer.parseInt(cellName.substring(1)) -1; //ex: B2's row index is 1 
     	this.col = Spreadsheet.getColumnNumberFromColumnLetter(cellName); //gets the letter
     }
 

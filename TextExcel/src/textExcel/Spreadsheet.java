aaed2ad97c2535647f15ab824b03e8782e46eb15
s-linji <s-linji@bsd405.org>
2018-01-09 15:18:46 -0800

    if regress, see //changed this

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -5,8 +5,8 @@ public class Spreadsheet implements Grid
 	private Cell[][] cellz;
 	public Spreadsheet()
 	{
-		cellz = new Cell[21][12];
-		for (int x = 0; x < 21; x++)
+		cellz = new Cell[20][12];
+		for (int x = 0; x < 20; x++)
 		{
 			for (int y = 0; y < 12; y++)
 			{
@@ -42,16 +42,17 @@ public class Spreadsheet implements Grid
 	{
 		//12 col x 20 row
 		String grid = "   |A         |B         |C         |D         |E         |F         |G         |H         |I         |J         |K         |L         |";
-		for (int row = 1; row <= 20; row++)
+		for (int row = 0; row < 20; row++)
 		{
-			if (row <= 9)
+			int rowname = row+1;
+			if (rowname <= 9)
 			{
-				grid += "\n" + row + "  |" ; //2 spaces
+				grid += "\n" + rowname + "  |" ; //2 spaces
 				//ex: (newline) 1  |
 			}
 			else
 			{
-				grid += "\n" + row + " |"; //only 1 space
+				grid += "\n" + rowname + " |"; //only 1 space
 			}
 			for (int col = 0; col < 12; col++)
 			{
@@ -81,7 +82,7 @@ public class Spreadsheet implements Grid
 			//cell inspection
 			//maybe use the getCell?
 			int column = getColumnNumberFromColumnLetter(command.substring(0, 1));
-			int row = Integer.parseInt(command.substring(1));
+			int row = Integer.parseInt(command.substring(1)) -1; //changed this
 			if (cellz[row][column].fullCellText().equals("          ")) //if this cell is empty
 			{
 				return "";
@@ -96,7 +97,7 @@ public class Spreadsheet implements Grid
 		{
 			String cellspecific = command.substring(command.indexOf(" ") + 1); //ex: clear A1 >> cellspecific = A1
 			int column = getColumnNumberFromColumnLetter(cellspecific.substring(0, 1));
-			int row = Integer.parseInt(cellspecific.substring(1));
+			int row = Integer.parseInt(cellspecific.substring(1)) -1; //changed this
 			
 			cellz[row][column] = new EmptyCell();
 			String result = getGridText();
@@ -106,7 +107,7 @@ public class Spreadsheet implements Grid
 		{
 			String[] split = command.split(" ");
 			int column = getColumnNumberFromColumnLetter(command.substring(0, 1));
-			int row = Integer.parseInt(split[0].substring(1));
+			int row = Integer.parseInt(split[0].substring(1)) - 1; //changed this
 			String input = command.substring(command.indexOf("=") + 3, command.length()-1);
 			
 			cellz[row][column] = new TextCell(input); 

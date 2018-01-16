6fe4c6f9e5ec3e26d89a746c2966e5e3a5a5c12f
s-linji <s-linji@bsd405.org>
2018-01-16 13:49:12 -0800

    sajfkdg

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -6,9 +6,10 @@ public class TextExcel
 	public static void main(String[] args)
 	{
 		Spreadsheet spreadsheet = new Spreadsheet();
+		Scanner console = new Scanner(System.in);
 		for (int x = 0; x < 100; x--)
 		{
-			Scanner console = new Scanner(System.in);
+			
 			String input = console.nextLine();
 			if (!input.equals("quit"))
 			{
